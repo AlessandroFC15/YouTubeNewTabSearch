8ea4923605c476ec34a47d1f4bea5fd844c83885
Alessandro <ale-remo@hotmail.com>
2018-01-27 18:56:12 -0300

    Bug-fixes :)

--- eventPage.js
+++ eventPage.js
@@ -1,4 +1,3 @@
-
 (function () {
     "use strict";
 
@@ -7,11 +6,11 @@
             isActive = false;
         }
 
-        chrome.tabs.create({ url: url, active: isActive, index: activeTabIndex + 1});
+        chrome.tabs.create({url: url, active: isActive, index: activeTabIndex + 1});
     };
 
     chrome.runtime.onMessage.addListener(
-        function(request, sender, sendResponse) {
+        function (request, sender, sendResponse) {
             openTabRightNextToActiveTab(request.url, sender.tab.index);
         });
 }());
--- main.js
+++ main.js
@@ -37,11 +37,11 @@
                 newButton = document.createElement('div');
                 newButton.style.cursor = "pointer";
                 newButton.style.width = "65px";
-                newButton.style.border = "1px solid var(--yt-searchbox-legacy-button-border-color)";
-                newButton.style.backgroundColor = "var(--yt-searchbox-legacy-button-color)";
+                newButton.style.border = "1px solid var(--ytd-searchbox-legacy-button-border-color)";
                 newButton.style.borderRadius = "0 2px 2px 0";
                 newButton.style.margin = "0";
                 newButton.style.textAlign = "center";
+                newButton.style.backgroundColor = "var(--ytd-searchbox-legacy-button-color)";
 
                 insertAfter(newButton, normalSearchButton);
             } else {
@@ -56,7 +56,10 @@
             }
 
             newButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 24 30" ' +
-                'enable-background="new 0 0 24 24" xml:space="preserve" style="width: 20px;height: 20px; margin-top: 6px"><g><path fill="#000000" d="M18.8,13.7c-0.2,0-0.4,0.2-0.4,0.4v6.2H3.6V5.8h6.3c0.2,0,0.4-0.2,0.4-0.4S10.1,5,9.9,5H3.2   C3,5,2.8,5.2,2.8,5.4v15.3c0,0.2,0.2,0.4,0.4,0.4h15.6c0.2,0,0.4-0.2,0.4-0.4v-6.6C19.2,13.9,19.1,13.7,18.8,13.7z"></path><path fill="#000000" d="M20.8,2.8h-6.7c-0.2,0-0.4,0.2-0.4,0.4s0.2,0.4,0.4,0.4h5.7L8.3,15.1c-0.2,0.2-0.2,0.4,0,0.6   c0.1,0.1,0.2,0.1,0.3,0.1s0.2,0,0.3-0.1L20.4,4.3V10c0,0.2,0.2,0.4,0.4,0.4s0.4-0.2,0.4-0.4V3.3C21.2,3,21,2.8,20.8,2.8z"></path></g></svg>';
+                'enable-background="new 0 0 24 24" xml:space="preserve" style="width: 20px;height: 20px; margin-top: 6px"><g>' +
+                '<path style="fill: var(--ytd-searchbox-legacy-button-icon-color);" d="M18.8,13.7c-0.2,0-0.4,0.2-0.4,0.4v6.2H3.6V5.8h6.3c0.2,0,0.4-0.2,0.4-0.4S10.1,5,9.9,5H3.2   C3,5,2.8,5.2,2.8,5.4v15.3c0,0.2,0.2,0.4,0.4,0.4h15.6c0.2,0,0.4-0.2,0.4-0.4v-6.6C19.2,13.9,19.1,13.7,18.8,13.7z"></path>' +
+                '<path style="fill: var(--ytd-searchbox-legacy-button-icon-color);" d="M20.8,2.8h-6.7c-0.2,0-0.4,0.2-0.4,0.4s0.2,0.4,0.4,0.4h5.7L8.3,15.1c-0.2,0.2-0.2,0.4,0,0.6   c0.1,0.1,0.2,0.1,0.3,0.1s0.2,0,0.3-0.1L20.4,4.3V10c0,0.2,0.2,0.4,0.4,0.4s0.4-0.2,0.4-0.4V3.3C21.2,3,21,2.8,20.8,2.8z"></path>' +
+                '</g></svg>';
 
             newButton.id = 'button-newTab';
             newButton.addEventListener('click', function () {
